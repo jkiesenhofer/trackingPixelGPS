54318ab28c28191ae37edf7c1dd377f8f59cdcbe
jkiesenhofer <179704619+jkiesenhofer@users.noreply.github.com>
2025-05-27 19:16:23 +0200

    Update pixelMap.py

--- pixelMap.py
+++ pixelMap.py
@@ -20,7 +20,7 @@ for i in range(0, 5):
     a=str(b[i]).split("'")[1]
     b[i]=a
 b=['213.162.73.172']+b[0:4]
-print(b)
+#print(b)
 # creating the dataframe
 df = pd.DataFrame({"IPV4": ['213.162.73.172', '109.199.115.41', '93.37.250.70','65.183.218.91', '65.183.218.92'],
                    "City": ['Vienna', 'Dusseldorf', 'Milano','Linz', 'Milano'],
