029392b4ef0d96cb0744d9e27ef3e7a299723f0c
jkiesenhofer <179704619+jkiesenhofer@users.noreply.github.com>
2025-05-28 11:27:25 +0200

    Add files via upload

---
+++ ipTracking.py
@@ -0,0 +1,23 @@
+"""----------------------------------------------------------------------------
+   Copyright (C) 2023 Lambda Optoelectronic Marketing Technologies e.U.
+----------------------------------------------------------------------------"""
+
+import pandas as pd
+import ipaddress as ip
+
+df = pd.read_csv('IParray.csv')
+array = df.values.tolist()
+
+a0=str(['213.162.73.172']).split("'")[1]
+
+a=array[0:(30)][:]
+print(a0)
+
+for k in range(0,len(a)):
+    b=str(a[k]).split("'")[1]
+    a[k]=b
+    c=int(ip.IPv4Address(a[k]))
+    print(b)
+q = lambda a:a+1
+
+print(q)
