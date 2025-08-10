8c12648c0fe2373d89b94830407eda973290d1a5
jkiesenhofer <179704619+jkiesenhofer@users.noreply.github.com>
2025-08-10 21:06:37 +0200

    Update auxiliaryTool.py

--- auxiliaryTool.py
+++ auxiliaryTool.py
@@ -1,23 +1,32 @@
-import xml.etree.ElementTree as ET
-import csv
+import pandas as pd
+import numpy as np
+import requests
+# Reading a XML file into a DataFrame
+df = pd.read_csv('SAPrules.xml')
 
-def xml_to_csv(xml_file, csv_file):
-    tree = ET.parse(xml_file)
-    root = tree.getroot()
-        
-    # Assuming all data is in a list of dictionaries
-    data = []
-    for element in root.findall("./row"): #Adjust path as needed
-        row = {}
-        for child in element:
-            row[child.tag] = child.text
-        data.append(row)
+array = df.values.tolist() # xml-list
+lines=1236
+b=array[0:lines]
 
-    if data:
-        with open(csv_file, 'w', newline='') as csvfile:
-            fieldnames = data[0].keys()
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writeheader()
-            writer.writerows(data)
+#print(b[10:20])
+n=df.to_string()
+#print(n)
 
-xml_to_csv('SAPrules.xml', 'output.csv')
+for i in range(lines):
+    a=str(b[i]).split('<transaction>')[0]
+    #print(str(i)+a)
+    
+#print(len(array))
+m=85
+#print(type(array))
+
+for j in range(m):
+
+    u = list(n.split('<transaction>')[j])
+    uu = list(n.split('<transaction>')[1:m])
+
+#print(u[0:(20)])
+
+    k="".join(str(x) for x in u[0:(20)])
+
+    print(k.split('<')[0])
