e0ccb884637f660cbc16d3d9f238b8e219f8a8c7
jkiesenhofer <179704619+jkiesenhofer@users.noreply.github.com>
2025-08-18 20:19:14 +0200

    Update ipTracking.py

--- ipTracking.py
+++ ipTracking.py
@@ -3,18 +3,17 @@ import ipaddress as ip
 
 df = pd.read_csv('IParray.csv')
 array = df.values.tolist()
+a0=list(str(['104.18::183.229']).split("::")[1])
 
-a0=str(['104.18.187.229']).split("::")[1]
-
-a=array[0:(30)][:]
+b=array[0:(30)][:]
+#b=df.to_string()
 print(a0)
 
-for k in range(0,len(a)):
-    b=str(a[k]).split("::")[1]
-    a[k]=b
-    c=int(ip.IPv4Address(a[k]))
-    print(b)
+for k in range(0,2):
+    b=str(a0[k]).split("s")[0]
+    a0[k]=list("".join(b))
+    #c=int(ip.IPv4Address(a0[k]))
+    print(a0)
 q = lambda a:a+1
 
 print(q)
-
