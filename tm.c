8feb0dd5df66a091b9b0b4dcaa88dee6d0736661
jkiesenhofer <179704619+jkiesenhofer@users.noreply.github.com>
2025-09-21 11:39:43 +0200

    Create tm.c

---
+++ tm.c
@@ -0,0 +1,32 @@
+// Header file for input output functions
+#include <stdio.h>
+
+// Main function: entry point for execution
+int main() {
+
+    // Writing print statement to print
+    printf("~");
+    
+    float abd;  // agency business document
+    float cass; // cargo account settlement
+    float crm;  // customer relationship management
+    float dtr;  // delivery-based transportation requirement
+    float ecc;  // erp core component
+    float fa;   // freight agreement
+    float fb;   // freight booking
+    float fo;   // freight order
+    float fsd;  // freight settlement document
+    float fu;   // freight unit
+    float fubr; // freight unit building rule
+    float fwo;  // forwarding order
+    float fwsd; // forwarding settlement document
+    float lsp;  // logistics service provider
+    float otr;  // order-based transportation requirement
+    float pi;   // process integration
+    float ses;  // service entry sheet
+    float trq;  // transportation requirement
+    float ui;   // user interface
+    float vsr;  // vehicle scheduling and routing
+    
+    return 0;
+}
