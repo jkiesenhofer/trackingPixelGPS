05418d9e074208859040c2f615e2f45900f0e052
jkiesenhofer <179704619+jkiesenhofer@users.noreply.github.com>
2025-09-21 18:22:54 +0200

    Update signal.java

--- optaphy/signal.java
+++ optaphy/signal.java
@@ -1,16 +1,27 @@
 /*-----------------------------------------------------------------------------
 -------------------------------------------------------------------------------
                             |
-         FM-Pool            |  Optaphy: Thermoelectric Control Logistics
+         FM-Pool            |  Optaphy: Thermoelectric Logistics Management
           9173              |  www.optaphy.com
                             |
 -------------------------------------------------------------------------------
    Copyright (C) 2024 Optaphy SE
 -------------------------------------------------------------------------------
 -----------------------------------------------------------------------------*/
-public interface signal {
-  void patch();
-} // predictor
-interface lambdaPrediction {
-  public void impedance();
-} // static
+public class SignalProcessor implements signal, lambdaPrediction {
+    @Override
+    public void patch() {
+        System.out.println("Patching signal...");
+    }
+
+    @Override
+    public void impedance() {
+        System.out.println("Calculating impedance...");
+    }
+
+    public static void main(String[] args) {
+        SignalProcessor processor = new SignalProcessor();
+        processor.patch();      // Calls the patch method
+        processor.impedance();  // Calls the impedance method
+    }
+}
